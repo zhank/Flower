f0127c38c10497f4ebbdc37d4c31006e385d25a7
liujp <1345526595@qq.com>
2017-04-01 21:34:59 +0800

    conflict

--- src/com/flower/main/panel/Login.java
+++ src/com/flower/main/panel/Login.java
@@ -1,6 +1,10 @@
 package com.flower.main.panel;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.Page;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -15,11 +19,10 @@ public class Login extends Panel {
 
 	private static final long serialVersionUID = 1L;
 
-	public Login(String id) {
+	public Login(final Page page, String id) {
 		super(id);
 		this.setOutputMarkupId(true);
-		this.add(creatLoginPanel());
-		
+		this.add(creatLoginPanel(page));
 		
 		this.add(new Label("htmlFooter", AppContextMgr.getHtmlFooter()));
 	}
@@ -29,9 +32,44 @@ public class Login extends Panel {
 	 * 
 	 * @return
 	 */
-	public Component creatLoginPanel() {
+	public Component creatLoginPanel(Page page) {
 		WebMarkupContainer loginWc = new WebMarkupContainer("loginWc");
 		loginWc.add(new Label("welTitle", new Model<String>("以琳花房欢迎您")));
+		
+		ModalWindow modal = new ModalWindow("modalWindow");
+		this.add(modal);
+		setModal(page, modal, "用户注册", new int[]{800, 400, 800, 400});
+		
+		//用户注册
+		AjaxLink<String> registLink = new AjaxLink<String>("rigistLin") {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				modal.show(target);
+			}
+		};
+		
+		loginWc.add(registLink);
 		return loginWc;
+	}
+	public void setModal(final Page page, ModalWindow modal, String title, int[] modalSize) {
+		modal.setInitialHeight(450);
+		modal.setInitialWidth(650);
+		modal.setMinimalHeight(450);
+		modal.setMinimalWidth(650);
+		modal.setTitle(title);
+		modal.setCssClassName("modalCss");
+		
+		modal.setPageCreator(new ModalWindow.PageCreator(){
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Page createPage() {
+				Page page = new RegistPage();
+				return page;
+			}
+		});
 	}
 }
